edf8a400685e1555fe863687bccddb454bfc9d23
AFPA1701871 <martine.poix@afpa.fr>
2025-10-23 11:16:21 +0200

    exemple boucle

--- 2. Algorithmie/Solution Python/Exemple boucles.py
+++ 2. Algorithmie/Solution Python/Exemple boucles.py
@@ -8,20 +8,33 @@
 # for i in range( len(liste)):
 #     print(liste[i])
 
-a=0
-while a<10:
-    a+=1
-    print(a)
+# a=0
+# while a<10:
+#     a+=1
+#     print(a)
 
 
-i = 1
-while i < 20:
-    if i==10 :
-        print("anniversaire dizaine")
-        i+=1   # il faut incrementer i pour eviter la c=boucle infinie
-        continue  # arrete ce tour de boucle et repend au while
-    if i==18:
-        print("majeur")
-        break  #arrete completement la boucle
-    print(f"anniversaire {i} ans")
-    i += 1
+# i = 1
+# while i < 20:
+#     if i==10 :
+#         print("anniversaire dizaine")
+#         i+=1   # il faut incrementer i pour eviter la c=boucle infinie
+#         continue  # arrete ce tour de boucle et repend au while
+#     if i==18:
+#         print("majeur")
+#         break  #arrete completement la boucle
+#     print(f"anniversaire {i} ans")
+#     i += 1
+
+# Remplir une liste
+# liste=[]
+# for i in range( 5):
+#     nb = int(input("Entrer un nombre : "))
+#     # liste[i] = nb
+#     liste.append(nb)
+# print(liste)
+
+liste=[]
+for i in range( 5):
+    liste.append(int(input("Entrer un nombre : ")))
+print(liste)
